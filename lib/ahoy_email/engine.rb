fc6d327bbd5539cbed2aa14fe24904b56fff416d
Ivan <anon@Ivans-iMac.local>
2023-12-24 13:03:30 -0800

    creds

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -4,16 +4,11 @@ module AhoyEmail
   class Engine < ::Rails::Engine
     initializer "ahoy_email" do |app|
       AhoyEmail.secret_token ||= begin
-        creds =
-          if app.respond_to?(:credentials) && app.credentials.secret_key_base
-            app.credentials
-          elsif app.respond_to?(:secrets)
-            app.secrets
-          else
-            app.config
-          end
-
-        creds.respond_to?(:secret_key_base) ? creds.secret_key_base : creds.secret_token
+        if app.respond_to?(:credentials) && app.credentials.secret_key_base
+          app.credentials.secret_key_base
+        else
+          raise "Rails credentials not set"
+        end
       end
     end
   end

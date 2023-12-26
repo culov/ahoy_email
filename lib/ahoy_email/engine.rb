936b3496fac5f9005fd7d04050cd334ebd65a49a
Ivan Pardo <ivanpardo@Ivans-MacBook-Pro.local>
2023-12-25 19:59:44 -0800

    OK

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -3,31 +3,18 @@ require "rails/engine"
 module AhoyEmail
   class Engine < ::Rails::Engine
     initializer "ahoy_email" do |app|
-     AhoyEmail.secret_token ||= begin
-        # Fix for issue with Mailkick and SECRET_KEY_BASE_DUMMY with Rails 7.1
-        # https://github.com/ankane/mailkick/pull/74
-
-        if Rails::VERSION::STRING.to_f >= 7.1# && ENV["SECRET_KEY_BASE_DUMMY"]
-          # TODO use for token in 3.0
+      AhoyEmail.secret_token ||= begin
+        # Use a key generator for Rails 7.1 and above
+        if Rails::VERSION::STRING.to_f >= 7.1
           app.key_generator.generate_key("ahoy_email")
+        else
+          # Fallback for older Rails versions
+          # Use Rails.application.credentials.secret_key_base if available
+          token = Rails.application.credentials.secret_key_base
+          # Fallback to Rails.application.secret_key_base if credentials are not set
+          token ||= Rails.application.secret_key_base
+          token
         end
-
-        # TODO remove in 3.0
-        # creds =
-        #   if app.respond_to?(:credentials) && app.credentials.secret_key_base
-        #     app.credentials
-        #   elsif app.respond_to?(:secrets)
-        #     app.secrets
-        #   else
-        #     app.config
-        #   end
-
-        # token = creds.respond_to?(:secret_key_base) ? creds.secret_key_base : creds.secret_token
-        # token ||= app.secret_key_base # should come first, but need to maintain backward compatibility
-        # token
-
-        token = "asdffg"
-        token
       end
     end
   end
